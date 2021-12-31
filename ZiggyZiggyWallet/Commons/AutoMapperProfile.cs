3ac2a8fcbeb174651397f7ff97baa2b86ebe1793
Amos Sunmbo Alagbe <sunboy005@gmail.com>
2021-12-31 10:08:37 +0100

    Currency Mapped

--- ZiggyZiggyWallet/Commons/AutoMapperProfile.cs
+++ ZiggyZiggyWallet/Commons/AutoMapperProfile.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using ZiggyZiggyWallet.DTOs;
+using ZiggyZiggyWallet.DTOs.Currency;
 using ZiggyZiggyWallet.DTOs.Users;
 using ZiggyZiggyWallet.Models;
 
@@ -9,6 +10,7 @@ namespace ZiggyZiggyWallet.Commons
     {
         public AutomapperProfile()
         {
+            //UserMapping
             CreateMap<AppUser, UserToReturn>();
             CreateMap<AppUser, RegisterSuccess>()
                 .ForMember(dest => dest.UserId, x => x.MapFrom(x => x.Id))
@@ -19,13 +21,20 @@ namespace ZiggyZiggyWallet.Commons
                 .ForMember(dest => dest.PhoneNumber, opt => opt.MapFrom(u => u.Phone));
             //.ForMember(dest => dest.Address, x => x.MapFrom(s => new Address { Street = s.Street, State = s.State, Country = s.Country }));
 
+            //Wallets Mapping
             CreateMap<WalletToAdd, Wallet>()
                 .ForMember(dest => dest.Name, opt => opt.MapFrom(u => u.Name));
-
-            
+                        
             CreateMap<Wallet, WalletToReturn>();
 
-            //CreateMap<Photo, PhotoToReturnDto>();
+            //Currencies Mapping
+            CreateMap<CurrencyToAdd, Currency>()
+                .ForMember(dest => dest.Name, opt => opt.MapFrom(u => u.Name))
+                .ForMember(dest => dest.ShortCode, opt => opt.MapFrom(u => u.ShortCode))
+                .ForMember(dest => dest.Abbrevation, opt => opt.MapFrom(u => u.Abbrevation));
+                        
+            CreateMap<Wallet, WalletToReturn>();
+
         }
     }
 
